9ac004d6e04b75101777351d7d443218d37d71f0
“isdilowizeline” <israel.diaz@wizeline.com>
2021-02-18 22:28:08 -0600

    Reformat App Component, add Theme, Add Global Styles, Add header component
    Signed-off-by: “isdilowizeline” <“israel.diaz@wizeline.com”>

--- src/components/App/App.component.jsx
+++ src/components/App/App.component.jsx
@@ -1,5 +1,5 @@
-import React, { useLayoutEffect } from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import React, {useLayoutEffect} from 'react';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
 import AuthProvider from '../../providers/Auth';
 import HomePage from '../../pages/Home';
@@ -9,50 +9,59 @@ import SecretPage from '../../pages/Secret';
 import Private from '../Private';
 import Fortune from '../Fortune';
 import Layout from '../Layout';
-import { random } from '../../utils/fns';
+import Header from '../Header';
+import {random} from '../../utils/fns';
+import {GlobalStyles} from '../../global';
+import {ThemeProvider} from 'styled-components';
+import {theme} from '../../theme';
 
 function App() {
-  useLayoutEffect(() => {
-    const { body } = document;
+    useLayoutEffect(() => {
+        const {body} = document;
 
-    function rotateBackground() {
-      const xPercent = random(100);
-      const yPercent = random(100);
-      body.style.setProperty('--bg-position', `${xPercent}% ${yPercent}%`);
-    }
+        function rotateBackground() {
+            const xPercent = random(100);
+            const yPercent = random(100);
+            body.style.setProperty('--bg-position', `${xPercent}% ${yPercent}%`);
+        }
 
-    const intervalId = setInterval(rotateBackground, 3000);
-    body.addEventListener('click', rotateBackground);
+        const intervalId = setInterval(rotateBackground, 3000);
+        body.addEventListener('click', rotateBackground);
 
-    return () => {
-      clearInterval(intervalId);
-      body.removeEventListener('click', rotateBackground);
-    };
-  }, []);
+        return () => {
+            clearInterval(intervalId);
+            body.removeEventListener('click', rotateBackground);
+        };
+    }, []);
 
-  return (
-    <BrowserRouter>
-      <AuthProvider>
-        <Layout>
-          <Switch>
-            <Route exact path="/">
-              <HomePage />
-            </Route>
-            <Route exact path="/login">
-              <LoginPage />
-            </Route>
-            <Private exact path="/secret">
-              <SecretPage />
-            </Private>
-            <Route path="*">
-              <NotFound />
-            </Route>
-          </Switch>
-          <Fortune />
-        </Layout>
-      </AuthProvider>
-    </BrowserRouter>
-  );
+    return (
+        <ThemeProvider theme={theme}>
+            <BrowserRouter>
+                <AuthProvider>
+                    <GlobalStyles/>
+                    <Header/>
+                    <Layout>
+                        <Switch>
+                            <Route exact path="/">
+                                <HomePage/>
+                            </Route>
+                            <Route exact path="/login">
+                                <LoginPage/>
+                            </Route>
+                            <Private exact path="/secret">
+                                <SecretPage/>
+                            </Private>
+                            <Route path="*">
+                                <NotFound/>
+                            </Route>
+                        </Switch>
+                        <Fortune/>
+                    </Layout>
+                </AuthProvider>
+            </BrowserRouter>
+        </ThemeProvider>
+
+    );
 }
 
 export default App;
