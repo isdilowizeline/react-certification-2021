77dfb3ad8d8037e501c786d50b5aee85c5b70103
“isdilowizeline” <israel.diaz@wizeline.com>
2021-02-19 20:25:57 -0600

    Add new components to Header Component
    Signed-off-by: “isdilowizeline” <“israel.diaz@wizeline.com”>

--- src/components/Header/Header.component.jsx
+++ src/components/Header/Header.component.jsx
@@ -1,26 +1,40 @@
 import React, {useRef, useState} from 'react';
 import Burger from '../Burger';
+import DarkMode from '../DarkMode';
+import LoginButton from '../LoginButton';
+import Search from '../Search';
 import Menu from '../Menu';
 import {useOnClickOutside} from '../../utils/hooks/clickOutside';
+import {Col, Container, Row} from 'reactstrap';
 
 function Header() {
-  const [open, setOpen] = useState(false);
-  const node = useRef();
-  useOnClickOutside(node, () => setOpen(false));
-  return (
-      <>
-        <div className="header container-fluid">
-          <div className="row">
-            <div className="col-sm">
-              <div ref={node}>
-                <Burger open={open} setOpen={setOpen}/>
-                <Menu open={open} setOpen={setOpen}/>
-              </div>
-            </div>
-          </div>
-        </div>
-      </>
-  );
+    const [open, setOpen] = useState(false);
+    const node = useRef();
+    useOnClickOutside(node, () => setOpen(false));
+    return (
+        <>
+            <Container fluid={true} className="header">
+                <Row>
+                    <Col xs="2">
+                        <div ref={node}>
+                            <Burger open={open} setOpen={setOpen}/>
+                            <Menu open={open} setOpen={setOpen}/>
+                        </div>
+                    </Col>
+                    <Col xs="2">
+                        <Search/>
+                    </Col>
+                    <Col xs={{size: 3, offset: 4}}>
+                        <DarkMode/>
+                    </Col>
+                    <Col xs={{size: 1}}>
+                        <LoginButton/>
+                    </Col>
+
+                </Row>
+            </Container>
+        </>
+    );
 }
 
 export default Header;
