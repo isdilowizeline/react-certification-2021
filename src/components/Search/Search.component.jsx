646bedb7fec3a4a75b7be7352e7f04666cd7963d
“isdilowizeline” <israel.diaz@wizeline.com>
2021-02-19 20:26:35 -0600

    Add Search Component
    Signed-off-by: “isdilowizeline” <“israel.diaz@wizeline.com”>

---
+++ src/components/Search/Search.component.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Input, InputGroup, InputGroupAddon, InputGroupText} from 'reactstrap';
+import {StyledSearch} from './Search.styles';
+
+const Search = () => {
+  return (
+      <StyledSearch>
+        <InputGroup>
+          <InputGroupAddon addonType="prepend">
+            <InputGroupText>
+              <i className="bi  bi-search"/>
+            </InputGroupText>
+          </InputGroupAddon>
+          <Input placeholder="Search"/>
+        </InputGroup>
+      </StyledSearch>
+  );
+};
+
+export default Search;
